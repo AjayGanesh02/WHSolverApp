eb3aec089aa7f041be2d16937ecae0aed86c946d
Ajay Ganesh <ajganesh2002@gmail.com>
2022-05-22 16:20:07 -0400

    implemented basic app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Board from './components/board/board';
+import Results from './components/results/results';
 
 function App() {
+
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Word Hunt Solver</h1>
+        <p>This site finds the possible words that can be made from a 4x4 grid of letters.<br/> It can be used for games in the style of the iMessage game Word Hunt.</p>
       </header>
+      <main>
+      <form>
+        <label>
+          Enter your board as a string of unseperated letters:<br/>
+          <input type="text" value={input} onChange={handleInput} />
+        </label>
+        <input type="submit" onChange={handleSubmit} />
+      </form>
+      <Board/>
+      <Results/>
+      </main>
     </div>
   );
 }
---
+++ src/components/results/results.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ResultCard from './resultCard'
+
+export default function Results() {
+  return (
+    <div>
+    Results
+    <ResultCard/>
+    </div>
+  )
+}
