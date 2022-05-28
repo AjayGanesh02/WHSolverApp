587c8eaa39c9f3ee10dcdcd2bf47986e2b08a8be
Ajay Ganesh <ajganesh2002@gmail.com>
2022-05-28 01:30:56 -0400

    Added sorting functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Results from './components/results/results';
-
+import Switch from "react-switch";
 import './app.scss';
 
 function App() {
@@ -10,9 +10,14 @@ function App() {
   const [input, setInput] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [results, setResults] = useState([]);
+  const [checked, setChecked] = useState(true);
 
   const handleInput = (e) => {
     setInput(e.target.value);
+  }
+
+  const handleToggle = () => {
+    setChecked(!checked);
   }
 
   const handleSubmit = async (e) => {
@@ -22,12 +27,12 @@ function App() {
     //clear previous results
     setResults([]);
     setInput("");
-    setSubmitted(true);
 
     //get results from API and assign
-    const response = await fetch(`${APIURL}${input}`);
+    const response = await fetch(`${APIURL}${input}` + (checked ? '&sort=true' : '&sort=false'));
     const jsonresponse = await response.json();
     setResults(jsonresponse.data);
+    setSubmitted(true);
   }
 
   return (
@@ -35,11 +40,11 @@ function App() {
       <header>
         <h1>Word Hunt Solver</h1>
         <h3>by Ajay Ganesh</h3>
-        <p>This site finds the possible words that can be made from a 4x4 grid of letters.<br /> 
-        It can be used for games in the style of the iMessage game Word Hunt.<br/>
-        <br/>
-        This site is open source! The React frontend code can be found <a href="https://github.com/AjayGanesh02/whsolverfrontend">here</a>, <br/> 
-        and the Python API code can be found <a href="https://github.com/AjayGanesh02/whsolverbackend">here</a>.</p>
+        <p>This site finds the possible words that can be made from a 4x4 grid of letters.<br />
+          It can be used for games in the style of the iMessage game Word Hunt.<br />
+          <br />
+          This site is open source! The React frontend code can be found <a href="https://github.com/AjayGanesh02/whsolverfrontend">here</a>, <br />
+          and the Python API code can be found <a href="https://github.com/AjayGanesh02/whsolverbackend">here</a>.</p>
       </header>
       <main>
         <div className='form'>
@@ -48,6 +53,13 @@ function App() {
               Enter your board as a string of 16 unseperated letters:<br />
               <input type="text" value={input} onChange={handleInput} />
             </label>
+            <br /><br />
+            <label>
+              Sort results by length:<br />
+              <Switch onChange={handleToggle} checked={checked} height={20} width={50} />
+            </label>
+
+            <br /><br />
             <input type="submit" value="Submit" />
           </form>
         </div>
